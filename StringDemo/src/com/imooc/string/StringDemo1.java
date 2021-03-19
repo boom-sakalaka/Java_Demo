aeb609d5d95192083046ed2f927d35541a731d2b
Gu <936285702@qq.com>
2021-03-19 23:06:39 +0800

    字符串String 类

---
+++ StringDemo/src/com/imooc/string/StringDemo1.java
@@ -0,0 +1,21 @@
+package com.imooc.string;
+
+public class StringDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Java 编程 基础";
+		System.out.println("字符串的长度是:" + str.length());
+		
+		// 取出字符 程 并输出
+		System.out.println(str.charAt(6));
+		
+		// 取出 子串 编程 基础
+		System.out.println(str.substring(5));
+		
+		// 取出 子串 编程
+		System.out.println(str.substring(5,7));
+		
+	}
+
+}
