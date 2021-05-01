b612a0f38fb64e6b18ec338e7136ca99e0c32a0e
Gu <936285702@qq.com>
2021-05-01 14:54:45 +0800

    jstl

--- jstl/WebContent/core.jsp
+++ jstl/WebContent/core.jsp
@@ -8,6 +8,30 @@
 <title>Insert title here</title>
 </head>
 <body>
+	<h1>${ requestScope.score }</h1>
+	<c:if test="${score >=60 }">
+		<h1 style="color:green">恭喜，通过测试</h1>
+	</c:if>
+	<c:if test="${score < 60 }">
+		<h1 style="color:red">对不起，再接再厉</h1>
+	</c:if>
 	
+	<c:choose>
+		<c:when test="${grade == 'A' }">
+			<h2>你很优秀</h2>
+		</c:when>
+		<c:when test="${grade == 'B' }">
+			<h2>不错</h2>
+		</c:when>
+		<c:when test="${grade == 'C' }">
+			<h2>水平一般</h2>
+		</c:when>
+		<c:when test="${grade == 'D' }">
+			<h2>需要努力</h2>
+		</c:when>
+		<c:otherwise>
+			<h2>一切随缘</h2>
+		</c:otherwise>
+	</c:choose>
 </body>
 </html>
---
+++ jstl/src/com/imooc/jstl/jstlServlet.java
@@ -0,0 +1,36 @@
+package com.imooc.jstl;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class jstlServlet
+ */
+@WebServlet("/jstl")
+public class jstlServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public jstlServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setAttribute("score",58);
+		request.setAttribute("grade", "E");
+		request.getRequestDispatcher("/core.jsp").forward(request, response);
+		
+	}
+
+}
