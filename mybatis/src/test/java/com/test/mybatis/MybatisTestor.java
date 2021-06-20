d1fb38753ea63d780099a1df01ad915d13a628c9
Gu <936285702@qq.com>
2021-06-20 11:09:25 +0800

    mybatis mapper 映射

--- mybatis/src/test/java/com/test/mybatis/MybatisTestor.java
+++ mybatis/src/test/java/com/test/mybatis/MybatisTestor.java
@@ -1,5 +1,7 @@
 package com.test.mybatis;
 
+import com.test.mybatis.entity.Goods;
+import com.test.mybatis.utils.MybatisUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +11,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
+import java.util.List;
 
 public class MybatisTestor {
     @Test
@@ -18,17 +21,47 @@ public class MybatisTestor {
         System.out.println("SqlSessionFactory 加载成功");
         SqlSession sqlSession = null;
 
-       try{
-           sqlSession = sqlSessionFactory.openSession();
-           // 测试用
-           Connection connection = sqlSession.getConnection();
-           System.out.println(connection);
-       }catch (Exception e){
-           e.printStackTrace();
-       }finally {
-           if(sqlSession != null){
-               sqlSession.close();
-           }
-       }
+        try {
+            sqlSession = sqlSessionFactory.openSession();
+            // 测试用
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void testMybatisUtils() {
+        // 代码健壮性考虑，需要try catch finally
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtils.openSession();
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MybatisUtils.closeSession(sqlSession);
+        }
+    }
+    @Test
+    public void testSelectAll() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtils.openSession();
+            List<Goods> list =  sqlSession.selectList("goods.selectAll");
+            for(Goods goods : list){
+                System.out.println(goods.getTitle());
+            }
+        }catch (Exception e){
+            throw  e;
+        }finally {
+            MybatisUtils.closeSession(sqlSession);
+        }
     }
 }
